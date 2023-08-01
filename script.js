3258424af201fa43b6f7c6d9127464b5e64122e9
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-08-01 15:59:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-function debonuce(func, wait= 20, immediate = true ) {
-  var timeout;
-  return function() {
-    var context = this;
-    args = arguments;
-    var later = function() {
-      timeout = null;
-      if(!immediate) func.apply(context, args);
 
-    };
-
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if(callNow) func.apply(context, args);
-  };
-}
 
 const sliderImages = document.querySelectorAll('.slide-in');
 
@@ -34,4 +18,4 @@ function checkSlide() {
   });
 }
 
-window.addEventListener('scroll', debonuce(checkSlide))
+window.addEventListener('scroll', checkSlide)
